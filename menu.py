050f7b60f51a4c62d116cff2ea94a163e681e4ed
Cem Aydin <cem.aydin@gmx.ch>
2014-08-14 01:54:37 +0200

    trying to add class for menu (WIP)

--- menu.py
+++ menu.py
@@ -1,17 +1,5 @@
 
-# Menu generation workflow
-# ------------------------
-#
-# The goal would be to generate the menus only once and reuse it
-# for the different pages.
-#
-# The idea to accomplish this would be to use a class for menus.
-# An instance should be created on global level, but nothing
-# should be generated at this point, since we first need to refresh
-# all the repos. (Therefor not using __init__.)
-# The class should provide a method to 'get' the respective menu.
-# This method should return the menu, creating it if it doesn't
-# exist yet.
+
 
 import os
 
@@ -19,130 +7,101 @@ import config
 
 from common import read_tb_lines
 
-def generate_menu_subpath(branch, repo_name, subpath, active_path):
 
-    # for the "base" menu we don't want to have the directory
-    # name in the path, but for the "repo"/other menus we do
-    if repo_name == config.BASE_REPO_NAME:
-        pre_dir=""
-    else:
-        pre_dir=repo_name
 
-    dir=os.path.join(config.GIT_WD, repo_name, subpath)
+class Menu:
 
-    dir_content=os.listdir(dir)
+    def __init__(self, branch, repo_name, active_path):
 
-    md_files=[]
-    for file in dir_content:
-        if file.endswith(config.MD_EXT):
-            md_files.append(file)
+        self.branch=branch
+        self.repo_name=repo_name
+        self.active_path=active_path
+        # (starting subdir)
+        self.curr_subdir=""
+        if repo_name == config.BASE_REPO_NAME:
+            self.pre_dir=""
+        else:
+            self.pre_dir=repo_name
+        # (starting tag)
+        self.menu='<ul>\n'
 
-    # filter
-    for file in md_files:
-        for keyword in config.MENU_EXCLUDE_FILES_MD:
-            if keyword in file:
-                md_files.remove(file)
+        self.generate_menu_recurse()
 
-    link_line_templ='<li><a {} href="{}">{}</a></li>\n'
 
-    # generate the menu entries
-    dir_menu=""
-    for idx, md_file in enumerate(sorted(md_files)):
+    def generate_menu_recurse(self):
 
-        filename_noext=os.path.splitext(md_file)[0]
+        # (get dir content)
+        self.dir_abs=os.path.join(config.GIT_WD, self.repo_name, self.curr_subdir)
+        print(self.dir_abs)
+        dir_content=os.listdir(self.dir_abs)
+        dir_content.sort()
+        print(dir_content)
+        for file_num, file in enumerate(dir_content):
 
-        # get the page title from Pandoc title block
-        full_filepath=os.path.join(dir, md_file)
-        tb_title_list=read_tb_lines(full_filepath)
-        print("TB TITLE LIST: ", tb_title_list)
+            if file.endswith(config.MD_EXT):
+                self.make_entry_link(file, file_num)
 
+            elif os.path.isdir(os.path.join(self.dir_abs, file)) and file not in config.EXCLUDE_DIRS:
+                # create submenu
+
+                self.menu=self.menu+'<li>'+file+'/'
+
+                # (re-set current subdir)
+                subdir_path_abs=os.path.join(self.dir_abs, self.curr_subdir)
+                subdir_path_rel=os.path.relpath(subdir_path_abs, os.path.join(config.GIT_WD, self.repo_name))
+                self.curr_subdir=subdir_path_rel
+
+                # (recurse)
+                submenu=self.generate_menu_recurse()
+
+                self.menu=self.menu+submenu+'</li>'
+
+        self.menu=self.menu+'</ul>'
+
+
+    def make_entry_link(self, filename_md, file_num):
+
+        filename_noext=os.path.splitext(filename_md)[0]
+
+        # set the href
+
+        if file_num == 0:
+            filename="index.html"
+        else:
+            filename=filename_noext+".html"
+
+        # --> use os.absdir (?) here
+        link_href="/"+os.path.join(self.pre_dir, self.curr_subdir, filename)
+
+        # set the link text
+
+        # (get the page title from Pandoc title block)
+        filepath_abs=os.path.join(self.dir_abs, filename_md)
+        # --> only read title here
+        tb_title_list=read_tb_lines(filepath_abs)
         if tb_title_list == []:
             # (use the filename as fallback)
-            page_title=filename_noext
+            link_text=filename_noext
         else:
-            page_title=tb_title_list[0]
+            link_text=tb_title_list[0]
 
-        # set the src name
-        if idx == 0:
-            src_name="index.html"
+        # set active class
+        if filepath_abs == self.active_path:
+            link_class='class="active"'
         else:
-            src_name=filename_noext+".html"
+            link_class=""
 
-        if full_filepath == active_path:
-            active_class='class="active"'
-        else:
-            active_class=""
+        # create link
+        link_line='<li><a {} href="{}">{}</a></li>\n'.format(link_class, link_href, link_text)
 
-        link_src="/"+os.path.join(branch, pre_dir, subpath, src_name)
+        self.menu=self.menu+link_line
 
-        link_line=link_line_templ.format(active_class, link_src, page_title)
-
-        dir_menu=dir_menu+link_line
-
-    return dir_menu
-
-
-
-def generate_menu_recurse(branch, repo_name, active_path, ul_classname="", dir=""):
-
-    # (tags)
-#    tag_dir="<li>{}</li>\n"
-
-    # (get md links)
-
-    if dir == "":
-        repo_name_id='id="repo-menu-'+repo_name+'"'
-    else:
-        repo_name_id=""
-
-    if ul_classname != "":
-        if repo_name in active_path:
-            format_class='class="'+ul_classname+' repo-menu-active"'
-        else:
-            format_class='class="'+ul_classname+'"'
-    else:
-        format_class=""
-
-    menu='<ul {} {}>\n'.format(repo_name_id, format_class)+generate_menu_subpath(branch, repo_name, dir, active_path)
-
-    # (dir content)
-    dir_abs=os.path.join(config.GIT_WD, repo_name, dir)
-
-    dir_content=os.listdir(dir_abs)
-
-    subdirs=[]
-    for file in dir_content:
-        filepath=os.path.join(dir_abs, file)
-        if os.path.isdir(filepath):
-            subdirs.append(file)
-    #print("SUBDIRS: ", subdirs)
-
-    for excl_dir in config.EXCLUDE_DIRS:
-        if excl_dir in subdirs:
-            subdirs.remove(excl_dir)
-
-    #print("SUBDIRS: ", subdirs)
-    for subdir in subdirs:
-            subdir_path_abs=os.path.join(dir_abs, subdir)
-            # (add entry for directory)
-            menu=menu+'<li>'+subdir+'/'
-
-            subdir_path_rel=os.path.relpath(subdir_path_abs, os.path.join(config.GIT_WD, repo_name))
-     #       print("SUBDIR PATH REL: ", subdir_path_rel)
-
-            submenu=generate_menu_recurse(branch, repo_name, active_path, "", subdir_path_rel)
-
-            menu=menu+submenu+'</li>'
-
-    # (add closing tag)
-    menu=menu+"</ul>\n"
-
-    return menu
 
 
 def generate_base_menu(branch, active_path):
 
-    base_menu=generate_menu_recurse(branch, config.BASE_REPO_NAME, active_path)
+    base_menu_inst = Menu(branch, config.BASE_REPO_NAME, active_path)
+    base_menu=base_menu_inst.menu
 
     return base_menu
 
@@ -165,40 +124,10 @@ def generate_repos_menu(branch, active_path):
             active_id=""
 
         menu=menu+'<h3 onclick="javascript:show_menu(this, \'repo-menu-{repo_name}\')">{repo_name}<span class=\"arrow\" id=\"{id}\"></span></h3>\n'.format(repo_name=repo, id=active_id)
-        
-        menu=menu+generate_menu_recurse(branch, repo, active_path, "repo-menu")
 
-    return menu
+        #menu=menu+generate_menu_recurse(branch, repo, active_path, "repo-menu")
 
+        repo_menu_inst = Menu(branch, repo, active_path)
+        repo_menu=menu+repo_menu_inst.menu
 
-class Menu:
-    
-    def __init__(self, type="base"):
-
-        self.generated=False
-        self.type=type
-        self.menu=""
-
-    def get_menu():
-
-        if self.generated:
-            return self.menu
-        else:
-            self.gen_menu()
-            return self.menu
-
-    def gen_menu():
-
-        if self.type == "base":
-            self.menu=generate_base_menu()
-            self.generated=True
-
-        elif self.type == "repos":
-            self.menu=generate_repo_menu()
-            self.generated=True
-
-        else:
-            print("Menu: Bad menu type, leaving.")
-
-
-#base_menu=Menu("base")
+    return repo_menu
